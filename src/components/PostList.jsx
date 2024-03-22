bc13a21e4b41c2fe9245e6c967368c6cdc8b0df7
HamidEslmai <73718974+hamideslami72@users.noreply.github.com>
2024-03-23 03:20:59 +0330

    edit post list

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react'
 import { PostContext } from '../context/PostContext';
+import { Link } from 'react-router-dom';
 
 function PostList() {
     const posts = useContext(PostContext);
   return (
     <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {posts.map((posts) => (
-              <article key={posts.id} className="flex flex-col items-start justify-between">
+              <article key={posts.id} className="flex flex-col items-start bg-gray-900 p-6 rounded-xl">
                 <div className="relative w-full">
                   <img
                     src={posts.imageUrl}
@@ -17,24 +18,23 @@ function PostList() {
                 </div>
                 <div className="max-w-xl">
                   <div className="mt-8 flex items-center gap-x-4 text-xs">
-                    <time dateTime={posts.datetime} className="text-gray-500">
+                    <time dateTime={posts.datetime} className="text-gray-50">
                       {posts.date}
                     </time>
                     <a
                       href={posts.category.href}
-                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                      className="relative z-10 rounded-full bg-gray-800 px-3 py-1.5 font-medium text-gray-50 hover:bg-gray-700"
                     >
                       {posts.category.title}
                     </a>
                   </div>
                   <div className="group relative">
-                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                      <a href={posts.href}>
-                        <span className="absolute inset-0" />
-                        {posts.title}
-                      </a>
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-50 group-hover:text-blue-400">
+                    <Link to={`/posts/`+posts.id} className="font-semibold leading-6">
+                        {posts.title}    
+                    </Link> 
                     </h3>
-                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{posts.description}</p>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-50">{posts.description}</p>
                   </div>
                 </div>
               </article>
